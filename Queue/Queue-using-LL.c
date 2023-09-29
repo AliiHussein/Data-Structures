f47b82cbef477c5032a6ad8a04035ab2ef8719a8
AliiHussein <aliunali@yahoo.com>
2023-09-29 18:31:46 +0300

    Implemented enqueue and dequeue using C

--- Queue/Queue-using-LL.c
+++ Queue/Queue-using-LL.c
@@ -1,12 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Node{
     int data;
     struct Node* next;
 };
 
+struct Queue{
+    struct Node* front;
+    struct Node* rear;
+};
+
+int isEmpty(struct Node * front){
+    return (front == NULL);
+}
+
+void enqueue(struct Node ** front, struct Node ** rear, int element){
+    // 1. create a new node
+    struct Node* newnode = (struct Node*)malloc(sizeof(struct Node));
+    // 2. fill its data
+    newnode->data = element;
+    // 3. Assign it next
+    newnode->next = NULL;
+    // 4. Assign top & rear
+    if(isEmpty(*front)){
+        *front = newnode;
+    }
+    else{
+        (*rear)->next = newnode;
+    }
+    
+    *rear = newnode;
+}
+
+int dequeue(struct Node ** front, int element){
+
+    if(isEmpty(*front)){
+        printf("Queue is Empty!\n");
+        return -1;
+    }
+    // 0. Save the value to return later
+    int value = (*front)->data;
+    // 1. Create a delNode pointer
+    struct Node* delNode = *front;
+    // 2. move the front one step
+    *front = (*front)->next;
+    // 3. delete the node
+    free(delNode);
+
+    return value;
+}
+
+void display(struct Node * front){
+    struct Node* temp = front;
+
+    while(temp != NULL){
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
 void main(){
-    struct Node* Front = NULL;
+    struct Queue q1 = {NULL, NULL}; // creating an empty queue
 
     int elemet;
 
@@ -14,13 +70,14 @@ void main(){
     while(options != 1){
         printf("\n");
         printf("Queue: "); 
-        display(Front);
+        display(q1.front);
         printf("Enter an option: ");
         scanf("%d", &options);
         switch (options)
         {
         case 1: break;
-        case 2: printf("Enqueue: "); scanf("%d", &elemet); push(&Front, elemet); break;
+        case 2: printf("Enqueue: "); scanf("%d", &elemet); enqueue(&(q1.front),&(q1.rear), elemet); break;
+        case 3: printf("Dequeue: %d\n", dequeue(&(q1.front), elemet)); break;
         default: options = 1; break;
         }
     }
